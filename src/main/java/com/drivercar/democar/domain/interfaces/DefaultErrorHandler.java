017faaf3ea8156f7dbc36e8bbcd6d11de8349c9f
Sandro M dos Santos <sandromarista@gmail.com>
2022-03-22 02:51:29 -0300

    Internacionalização - messages

--- src/main/java/com/drivercar/democar/domain/interfaces/DefaultErrorHandler.java
+++ src/main/java/com/drivercar/democar/domain/interfaces/DefaultErrorHandler.java
@@ -3,24 +3,37 @@ package com.drivercar.democar.domain.interfaces;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentConversionNotSupportedException;
 
 @RestControllerAdvice
 public class DefaultErrorHandler {
+	@Autowired
+	private MessageSource messageSource;
 	
 	@ExceptionHandler(MethodArgumentConversionNotSupportedException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorResponse handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
 		List<ErrorData> messages = ex
 				.getBindingResult()
 				.getFieldErrors()
 				.stream()
-				.map(fe -> new ErrorData(fe.getDefaultMessage()))
+				.map(this::getMessage)
 				.collect(Collectors.toList());
 	
 		return new ErrorResponse(messages);
+	}
+	
+	private ErrorData getMessage(FieldError fieldError) {
+		return new ErrorData(messageSource.getMessage(fieldError, LocaleContextHolder.getLocale()));
 	}
 
 }
---
+++ src/main/java/com/drivercar/democar/domain/interfaces/LocaleResolver.java
@@ -0,0 +1,30 @@
+package com.drivercar.democar.domain.interfaces;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+
+@Component
+public class LocaleResolver extends AcceptHeaderLocaleResolver{
+	
+	private static final Locale DEFAULT_LOCALE  = new Locale("pt", "BR");
+	private static final List<Locale> ACCEPTED_LOCALES  = Arrays.asList(DEFAULT_LOCALE, new Locale("en"));
+	
+	@Override
+	public Locale resolveLocale(HttpServletRequest request) {
+		final String acceptLanguageHeader = request.getHeader("Accept-Language");
+		if(StringUtils.isBlank(acceptLanguageHeader) || acceptLanguageHeader.trim().equals("*")) {
+			return DEFAULT_LOCALE;
+		}
+	    List<Locale.LanguageRange> list = Locale.LanguageRange.parse(acceptLanguageHeader);
+	    Locale locale = Locale.lookup(list, ACCEPTED_LOCALES);
+	    return locale;
+	}
+
+}
